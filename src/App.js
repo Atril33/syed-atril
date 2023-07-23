d722a7aba50088e7b9ebf2a7efea6443ae516fbc
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-07-23 13:32:33 +0500

    Add Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import './Components/Style/style.css';
 import ProjectInfo from './Components/ProjectInfo';
 import Home from './Components/Home';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
+    <>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/project/:name" element={<ProjectInfo />} />
     </Routes>
+    <Analytics />
+    </>
   );
 }
 

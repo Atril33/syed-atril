44a97acd6a6d6a92c03c2b5c97b13db22408b7a8
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-11-11 20:23:06 +0500

    Update Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ReactGA from "react-ga";
 import Home from './Components/Home';
 
 
-const TRACKING_ID = "UA-292664246-1";
+const TRACKING_ID = "G-VP7DB551DG";
 ReactGA.initialize(TRACKING_ID)
 
 function App() {
